f70db79964537ae40f3cf9f55e7145843208a8ee
Brayden Price <brayden@braydenprice.dev>
2023-05-25 18:25:19 -0400

    Setup Firebase Analytics and Performance.

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <script setup>
 import { TopNavbar } from "#components";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCk6v3DvsnIOc-j5RM66Gy5h72cpbXqWhM",
+    authDomain: "gray-area-knives-v2.firebaseapp.com",
+    projectId: "gray-area-knives-v2",
+    storageBucket: "gray-area-knives-v2.appspot.com",
+    messagingSenderId: "382382782500",
+    appId: "1:382382782500:web:eb53919948c0a2dc06c0b1",
+    measurementId: "G-BX57GN8XMN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+// Import Web Vitals for Performance Insights
+import { onLCP, onFID, onCLS } from 'web-vitals';
 </script>
 
 <template>
